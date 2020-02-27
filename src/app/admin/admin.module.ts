d4dba40950d62b6f8664cb85857b7374a3b63342
Error <fearerror@yandex.ru>
2020-02-27 15:03:50 +0300

    Admin module.

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
+import {AdminRoutingModule} from './admin.routing.module';
+import {RouterModule} from '@angular/router';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {CreatePageComponent} from './create-page/create-page.component';
+import {EditPageComponent} from './edit-page/edit-page.component';
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ],
+  exports: [RouterModule]
+})
+
+export class AdminModule {
+
+}
