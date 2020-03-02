312af21fd62dbebc73af5126579a51716a42f268
Error <fearerror@yandex.ru>
2020-03-02 20:19:29 +0300

    Views posts.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {QuillModule} from 'ngx-quill-v2';
+import {HttpClientModule} from '@angular/common/http';
+import {QuillModule} from 'ngx-quill';
 
 @NgModule({
   imports: [HttpClientModule, QuillModule.forRoot()],
